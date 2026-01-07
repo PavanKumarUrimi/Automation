d11d7c4fd1957c72283aed6a54c537296058ce32
URIMI Pavan Kumar <pavan.kumar.urimi@hexagon.com>
2026-01-07 14:21:25 +0530

    Improved validations

--- Invokescript.ps1
+++ Invokescript.ps1
@@ -1,14 +1,28 @@
 ﻿function Invoke-BashSum {
     param (
-        [int]$Num1,
-        [int]$Num2
+        [Parameter(Mandatory = $true)]
+        $Num1,
+        [Parameter(Mandatory = $true)]
+        $Num2
     )
+    try {
+        # Validate and convert inputs to integers
+        $Num1 = [int]$Num1
+        $Num2 = [int]$Num2
 
-    # Capture output from bash
-    $result = bash ./TestBash.sh $Num1 $Num2
+        # Capture output from bash
+        $result = bash ./TestBash.sh $Num1 $Num2
+    }
+    catch {
+        Write-Host "ERROR: Either inputs are not integers or bash script failed." -ForegroundColor Red
+        exit 1  
+    }
 
     Write-Host "OutPut from Powershell" $result
 }
 
 # Function call
-Invoke-BashSum -Num1 10 -Num2 20
+Invoke-BashSum -Num1 $Num1 -Num2 $Num2
+
+
+
